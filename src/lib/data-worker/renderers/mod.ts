fbff1d93deafeb09dd1a38424bb5d63feebccc1d
Michal Grňo <m93a.cz@gmail.com>
2023-08-22 23:31:28 +0200

    fix(RenderJob): remove the fields from JS too

--- src/lib/data-worker/renderers/mod.ts
+++ src/lib/data-worker/renderers/mod.ts
@@ -25,10 +25,6 @@ export interface RenderJob {
   darkMode?: boolean;
   renderAxes?: boolean;
   renderGrid?: boolean;
-
-  margin?: number;
-  xLabelSpace?: number;
-  yLabelSpace?: number;
 }
 
 export interface RenderJobResult {
