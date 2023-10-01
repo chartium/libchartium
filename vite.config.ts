82e42edfc53966bdaee30bba4d16dcebe4ec2937
Michal Grňo <m93a.cz@gmail.com>
2023-10-01 12:24:42 +0200

    build: fix the build (for real now), bump version to 0.0.4

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,8 @@
 import { defineConfig } from "vite";
 import { svelte } from "@sveltejs/vite-plugin-svelte";
 import { resolve } from "path";
-import dts from "vite-plugin-dts";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [svelte(), dts()],
-  build: {
-    lib: {
-      entry: resolve(__dirname, "src/lib/index.ts"),
-      formats: ["es"],
-      fileName: "main",
-    },
-  },
+  plugins: [svelte()],
 });
