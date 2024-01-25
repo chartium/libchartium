f4221c6b0bb42cf39fe16d7bd62c4cde8778d982
Richard Ivanek <richard.ivanek@gmail.com>
2024-01-25 15:36:45 +0100

    chore: update dependencies

--- src/rust/src/trace/mod.rs
+++ src/rust/src/trace/mod.rs
@@ -1,10 +1,11 @@
+mod batch;
+mod bundle;
 pub mod extensions;
 mod metas;
-mod bundle;
-mod batch;
 mod traceops;
 
-pub use metas::*;
-pub use bundle::*;
 pub use batch::*;
+pub use bundle::*;
+pub use metas::*;
+#[allow(unused_imports)]
 pub use traceops::*;
