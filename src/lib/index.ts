41f52750d3bb47c955a1ca252251bea0cff3e970
m93a <m93a.cz@gmail.com>
2024-06-16 08:13:04 +0200

    feat: wake up from a collective hallucination

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,4 +1,17 @@
 export * from "./data-worker/index.js";
 export * from "./components/index.js";
 export { NumericDateRepresentation } from "./utils/numericDateRepresentation.js";
-export type { ChartValue, Range } from "./types.js";
+export type {
+  ChartValue,
+  Range,
+  DateRange,
+  QuantityRange,
+  NumericRange,
+} from "./types.js";
+export {
+  isRange,
+  isDateRange,
+  isNumericRange,
+  isQuantity,
+  isUnit,
+} from "./types.js";
