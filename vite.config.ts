7082be43c0a0e2d8aaa3cf72a272f8d36f9ab0a1
m93a <m93a.cz@gmail.com>
2024-10-21 13:57:04 +0200

    feat: render a bundle size breakdown in dist/stats.html by `p build && p dlx vite build`

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,21 @@
 import { defineConfig } from "vite";
 import { svelte } from "@sveltejs/vite-plugin-svelte";
 import wasm from "vite-plugin-wasm";
+import { visualizer } from "rollup-plugin-visualizer";
+import topLevelAwait from "vite-plugin-top-level-await";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [svelte(), wasm()],
+  plugins: [
+    svelte(),
+    wasm(),
+    topLevelAwait({
+      promiseExportName: "__tla",
+      promiseImportName: (i) => `__tla_${i}`,
+    }),
+    visualizer({
+      emitFile: true,
+      gzipSize: true,
+      filename: "stats.html",
+    }),
+  ],
 });
